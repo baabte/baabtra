5f75f324c8ab6e01aa4ed636356583711d4ca91f
Jihin <jihin@baabte.com>
2014-11-27 11:41:32 +0530

    Commiting Basic modules by jihin

--- app.js
+++ app.js
@@ -25,7 +25,50 @@
         'oc.lazyLoad',
         'ui.load',
         'ui.jp',
-        'angular-loading-bar'
-      ]);
+        'angular-loading-bar',
+        'ui.tree'
+      ])
+
+       .config(
+        ['$stateProvider', '$urlRouterProvider',
+          function ( $stateProvider,   $urlRouterProvider ) {
+            $stateProvider
+              .state('baabtra', {
+                url: '/baabtra',
+                views: {
+                  // So this one is targeting the unnamed view within the parent state's template.
+                  '': {
+                    templateUrl: 'views/layout.html'
+                  },
+                  // This shows off how you could populate *any* view within *any* ancestor state.
+                  // Oopulating the ui-view="aside@"
+                  'aside': {
+                    templateUrl: 'views/partials/aside.nav.pages.html'
+                  }
+                }
+              })
+                .state('baabtra.roleMenuMapping', {
+                url: '/roleMenuMapping',
+                templateUrl: 'angularModules/roleMenuMapping/partials/Partial-roleMenuMapping.html',
+                controller: 'RoleMenuMappingCtrl'
+              })
+                .state('baabtra.userMenuMapping', {
+                url: '/userMenuMapping',
+                templateUrl: 'angularModules/company/partials/Partial-userMenuMapping.html',
+                controller: 'UsermenumappingCtrl'
+              })
+                .state('baabtra.JobPosting', {
+                url: '/JobPosting',
+                templateUrl: 'angularModules/company/partials/Partial-JobPosting.html',
+                controller: 'JobpostingCtrl'
+              })
+                .state('baabtra.ViewJobs', {
+                url: '/ViewJobs',
+                templateUrl: 'angularModules/company/partials/Partial-ListJobs.html',
+                controller: 'ListjobsCtrl'
+              });
+          }
+        ]
+      );
 
 }());
