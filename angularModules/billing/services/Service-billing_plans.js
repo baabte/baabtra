df2ac8cff31fea2c64acbce62debcdf11e3c16a8
midhun123 <midhun@baabte.com>
2014-12-18 10:21:18 +0530

    committing changes

--- angularModules/billing/services/Service-billing_plans.js
+++ angularModules/billing/services/Service-billing_plans.js
@@ -16,4 +16,41 @@ angular.module('baabtra').service('billingPlans',function($http,bbConfig) {
 	 		console.log(data);
 	 	});  
 	 };
+
+	 this.addNewBillingPlan=function ($scope){ // functon that call web service to load a feature
+	 	$http({
+	 		url: bbConfig.BWS+'addNewBillingPlan/',
+	 		data: angular.toJson({"NewPaln":$scope.newPlan}),
+	 		method: "POST",
+	 		withCredentials: false,
+	 		contentType:"application/json",
+	 		dataType:"json",
+	 	}).
+	 	success(function(data, status, headers, config) {
+                   $scope.fnaddNewBillingPlanBack(data);
+               }).
+	 	error(function(data, status, headers, config) {
+	 		// console.log(data);
+	 	});  
+        // console.log(angular.toJson($scope.newPlan));
+	 };
+
+	 this.retriveCurrentPlans=function ($scope){ // functon that call web service to load a feature
+	 	$http({
+	 		url: bbConfig.BWS+'retriveCurrentPlans/',
+	 		data: angular.toJson({"nothing":"nothing"}),
+	 		method: "POST",
+	 		withCredentials: false,
+	 		contentType:"application/json",
+	 		dataType:"json",
+	 	}).
+	 	success(function(data, status, headers, config) {
+                   $scope.fnretrieveCurrentPlans(data);
+               }).
+	 	error(function(data, status, headers, config) {
+	 		// console.log(data);
+	 	});  
+        // console.log(angular.toJson($scope.newPlan));
+	 };
+	 
 });
