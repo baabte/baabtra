940d406c96838ec6cd1eb930a4de432749967759
lijin <lijin@baabte.com>
2015-01-09 18:48:28 +0530

    course elements by jihin

--- bower_components/contextMenu/contextMenu.js
+++ bower_components/contextMenu/contextMenu.js
@@ -1,16 +1,20 @@
 angular.module('ui.bootstrap.contextMenu', [])
 
-.directive('contextMenu', ['$parse',function ($parse) {
+.directive('contextMenu', ['$parse','$state','$aside','$templateCache',function ($parse,$state,$aside,$templateCache) {
     var renderContextMenu = function ($scope, event, options) {
         if (!$) { var $ = angular.element; }
         if(options.length<1){
             return 0;
         }
+        console.log($scope.$parent.syncData);
+        var state=$state.current.name.split('.');
+        state=state[state.length-1];
         
-        console.log(options.step2.courseElementlist);
+        console.log(options[state].courseElementlist);
         $(event.currentTarget).parent().parent().parent().parent().addClass('context');
         var $contextMenu = $('<div>');
         $contextMenu.addClass('dropdown clearfix');
+        $contextMenu.css({height:'0px !important'});
         var $ul = $('<ul>');
         $ul.addClass('dropdown-menu');
         $ul.attr({ 'role': 'menu' });
@@ -22,9 +26,9 @@ angular.module('ui.bootstrap.contextMenu', [])
         });
         var $headerA = $('<span>');
              $headerA.text($scope.ddlBindObject[$scope.selectedDuration-1].name.replace('s','')+" "+$scope.$parent.tlpoint);
-             $headerA.addClass('font-bold bg-cadetblue p-xs col-xs-12');
+             $headerA.addClass('font-bold '+options[state].colorClass+' p-xs col-xs-12');
              $ul.append($headerA);
-        angular.forEach(options.step2.courseElementlist, function (item, i) {
+        angular.forEach(options[state].courseElementlist, function (item, i) {
             var $li = $('<li>');
             if (angular.equals(item, null)) {
                 $li.addClass('divider');
@@ -46,7 +50,24 @@ angular.module('ui.bootstrap.contextMenu', [])
                     $scope.$apply(function () {
                         $(event.currentTarget).parent().parent().parent().parent().removeClass('context');
                         $contextMenu.remove();
-                        console.log(item);
+                        console.log($scope);
+                        $scope.itemTemplate=item.courseElementTemplate;
+                        $templateCache.put('course-element-popup.html','<div style="padding: 0px;" class="aside col-xs-6 m-h-n b-l" role="dialog">'
+    +'<div class="box">'
+    +'<div class="p '+options[state].colorClass+' font-bold">'
+      +'<a ng-click="$hide()" class="pull-right text-white"><i class="fa fa-times"></i></a>'
+      +'<i class="fa '+ item.Icon +' text-md m-r"></i>'
+      +item.menuDisplayName
+    +'</div>'
+    +'<div class="box-row">'
+      +'<div class="box-cell m-t">'
+        +'<div class="box-inner col-xs-12">'
+          +'<form novalidate xt-form class="form" name="courseElement">'
+           +'<div fg-form fg-form-data="myFormData" form-data = "formData" fg-schema="itemTemplate"> </div>{{courseElement.$invalid}} : valid: {{$scope}} :TOOLTIP  : {{$scope.errTooltip}}'
+           +'<button type="submit" ng-click="" style="color:#fff!important;" ng-disabled = "courseElement.$invalid || !valid" class="pull-right btn '+options[state].colorClass+'">Save</button>'
+          +'</form>'
++'</div></div></div></div></div>');
+ $aside({scope: $scope, template:'course-element-popup.html', html:true});
                         //item.call($scope,$scope.$parent.tlpoint/$scope.ddlBindObject[$scope.selectedDuration-1].mFactor);
                     });
                 });
