64fcb070f804e7582ecf10ed33a56ea1931b0356
Lijin <lijin@baabte.com>
2015-01-12 10:32:26 +0530

    replaced hardcoded values in user registration - Lijin

--- angularModules/user/partials/Partial-userRegistration.js
+++ angularModules/user/partials/Partial-userRegistration.js
@@ -12,9 +12,9 @@ if($rootScope.loggedIn===false){
 $scope.currentState=$state.current.name;
 // console.log($rootScope.userinfo);
  var loggedusercrmid=$rootScope.userinfo.ActiveUserData.roleMappingId.$oid;
- var companyId="5457526122588a5db73e0b23";
+ var companyId=$rootScope.userinfo.ActiveUserData.roleMappingObj.fkCompanyId.$oid;//"5457526122588a5db73e0b23";
  // $rootScope.userinfo.ActiveUserData.roleMappingObj.fkCompanyId.$oid;
-branchSrv.fnLoadBranch($scope,'5457526122588a5db73e0b23');
+branchSrv.fnLoadBranch($scope,companyId);
 $scope.tableData={};
 $scope.formData={};
 $scope.newUser=false;
