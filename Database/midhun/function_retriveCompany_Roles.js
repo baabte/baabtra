9459c6ac1fe686c3ae70528e602548f7eb0f5589
midhun123 <midhun@baabte.com>
2015-01-16 15:20:59 +0530

    changes in roles

--- Database/midhun/function_retriveCompany_Roles.js
+++ Database/midhun/function_retriveCompany_Roles.js
@@ -1,8 +1,14 @@
 db.system.js.save(
 	{
-		_id:"function_loadFeatures",
-		value:function(value)
+		_id:"function_retriveCompany_Roles",
+		value:function(data)
 		{
-			companyId=db.ClnRoleMaster.find({"companyId":ObjectId(value),activeFlag:1}).toArray();
-			return companyId;
+			if(data.usertype==1){
+				roles=db.clnRoleMaster.find( { companyId: { $exists: false },activeFlag:1 } ).toArray();
+				return roles;
+			}else{
+				roles=db.clnRoleMaster.find({"companyId":ObjectId(data.companyId),activeFlag:1}).toArray();
+				return roles;
+			}
+			
 	}});
