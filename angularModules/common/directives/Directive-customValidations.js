b84922e590b19477d544f4ad9b1bd7d8897f709a
Anoop <anoop@baabte.com>
2015-01-20 10:30:08 +0530

    Updated the final version of validateoneinmany directive in customValidations.js

--- angularModules/common/directives/Directive-customValidations.js
+++ angularModules/common/directives/Directive-customValidations.js
@@ -1,72 +1,5 @@
 angular.module('baabtra')
 
-//To add validation indicators to respective controls (Test)
-.directive('indicateValTest', function() {
-	return {
-		restrict: 'A',
-
-		link: function(scope, elem, attrs, fn) {		
-					
-					
-
-			    // A variable to hold the current element
-				scope.thisElem;
-				scope.$watch('scope.thisElem', function(){
-
-								
-			
-					// the array to hold the symbols
-					scope.symbolCollection = {
-						'required': 'ti-star text-danger',
-						'email':'ti-email text-info',
-						'Number':'ti-mobile text-info'
-					};
-
-
-
-					// Looping through all the controls in the form and attaching the specific symbol	
-					for(var i = 0; i <= elem.context.length; i++){
-
-						
-
-						var add = false;
-						scope.thisElem = elem.context[i];
-
-						$(scope.thisElem).parent().attr('class', 'input-group m-b col-xs-12');
-						scope.icon=$('<span class="input-group-addon"></span>');
-
-						
-						if($(scope.thisElem).attr('required') && angular.equals($(scope.thisElem).attr('type'),'text')){
-							scope.icon.addClass(scope.symbolCollection['required']);
-							add=true;
-						}
-
-
-						if ($(scope.thisElem).attr('ng-pattern')){
-
-							var pattern = $(scope.thisElem).attr('ng-pattern');
-							pattern = pattern.replace('Pattern','');
-							
-							scope.icon.removeClass(scope.symbolCollection['required']);
-							scope.icon.addClass(scope.symbolCollection[pattern]);
-							add=true;
-						}
-
-
-						
-						if(add){
-							$(scope.thisElem).parent().prepend(scope.icon);
-						}
-						
-
-					}
-			}); 
-		} 
-	  
-	};
-
-})
-
 //Add scope.icon to individual controls
 .directive('indicateVal', function() {
 	return {
@@ -135,7 +68,7 @@ angular.module('baabtra')
 		restrict: 'A',
 		require: ["^?form",'ngModel'],
 		link: function(scope, elem, attrs, ctrls) {
-			
+
 			//checking for the existence of the "validation-group" attribute and throwing the error
 			if(!attrs.validationGroup){
 				throw new Error('The "validate-one-in-many" directive needs a "validation-group" attribute to work properly');
@@ -153,58 +86,60 @@ angular.module('baabtra')
 			scope.validationGroups[attrs.validationGroup]=[];
 			}
 			scope.validationGroups[attrs.validationGroup].push(ctrls[1]);
-			//.End	
+			//.End
 
-			//defining a variable to dtermine whether to set all the controls in the validation group to be valid
-			scope.setAllRequired = true;		
-			
-			
 
 			//binding a change event to validate when the text changes
 			scope.$watch(function (){return elem.context.value;/* define what to watch*/
 }, function(){				
 			
 
-				//defining a variable to hold the valid state for the validation group
-				scope.vgValid = false;
-				
-
 				//defining a variable to hold the validation group name
-				scope.vgName = attrs.validationGroup;;
-				//defining a variable to hold the source control which initiated the validation
-				scope.srcControl = ctrls[1];				
+				scope.vgName = attrs.validationGroup;
 				
-				//if the current element is valid
-				if(!ctrls[1].$error.required) {		
-
-					scope.vgValid = true;													
 					
-                } //.End if(!ctrl.$invalid)
+					//if the current element is valid
+					if(!ctrls[1].$error.required) {	
+
+						// if the control is valid setting the other controls in the same validation group to valid
+						for (var i = 0; i < scope.validationGroups[scope.vgName].length; i++ ){	
+
+								scope.validationGroups[scope.vgName][i].$setValidity("required", true);
+						}
+																		
+						
+	                } else{
+
+	                	var setAllRequired = true;                	
+
+	                	// checking whether the value of the element i null, if so validate other controls and set the validity to true if any of the other fields holds a value
+	                	if(angular.equals(elem.context.value.trim(),"") && !ctrls[0].$pristine)	{
+
+	                		for (var i = 0; i < scope.validationGroups[scope.vgName].length; i++ ){	
+	                			if(!angular.equals($( "input[name='" + scope.validationGroups[scope.vgName][i].$name + "']" ).val().trim(), "")){
+	                				setAllRequired = false;
+	                			}	                		
+
+	                		} //.End for (var i = 0; i < scope.validationGroups[scope.vgName].length; i++ )
+
+	                		// 2nd loop to set the controls valid or invalid in accordance with the setAllRequiredVariable
+	                		for (var i = 0; i < scope.validationGroups[scope.vgName].length; i++ ){
+	                			
+	                			scope.validationGroups[scope.vgName][i].$setValidity("required", !setAllRequired);
+
+	                		} //.End 2nd loop
+								
+						} //.End if(angular.equals(elem.context.value.trim(),""))
+
+	                }
+
+	                //.End if(!ctrl.$invalid)
+
+                // }
+
+			});// .End scope.$watch(function (){return elem.context.value	
 
 
-
-			});// .End scope.$watch(function (){return elem.context.value
-
-			// Setting a watch function on the scope.vgName + vgValid to toggle the validity
-			scope.$watch(function (){return (scope.vgName + scope.vgValid);/* define what to watch*/
-}, function(){	
-
-			
-			//check if vgValid is true to run the function
-			if(angular.equals(attrs.validationGroup, scope.vgName)){
-				if(scope.vgValid) { // && !angular.equals(scope.srcControl.$name,ctrls[1].$name) ) {
-					//setting the validity state of the controller
-					ctrls[1].$setValidity("required", true);
-				} 
-				else{
-					//setting the validity state of the controller
-					ctrls[1].$setValidity("required", false);
-				}
-			}	
-				
-			});
-			// .End Setting a watch function on the vgValid element		
-		        
 		} //.End link
 	  
 	}//. End return
