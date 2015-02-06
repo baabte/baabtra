cc2d3992127f73cb4a4e5eea1be79872c66a1605
Jihin <jihin@baabte.com>
2015-02-06 15:51:15 +0530

    courseStreamLine,addCourse by jihin

--- angularModules/courseStreamline/directives/Directive-courseStreamline.js
+++ angularModules/courseStreamline/directives/Directive-courseStreamline.js
@@ -1,16 +1,11 @@
 angular.module('baabtra').directive('courseStreamline', function() {
 	return {
 		restrict: 'E',
-		replace: true,
 		scope: {
 			previewData:'='
 		},
 		templateUrl: 'angularModules/courseStreamline/directives/Directive-courseStreamline.html',
 		link: function(scope, element, attrs, fn) {
-			console.log(scope);
-			scope.$watch("previewData", function(){
-				scope.previewDatas = scope.previewData;
-			},true);
 		}
 	};
 });
