44a9e0338fb2520e52deef35d8e58783d12af699
Arun R Menon <arun@baabte.com>
2015-03-02 20:53:43 +0530

    commit to pull the latest by arun

--- angularModules/questionRelated/questionGroup/directives/Directive-questionGroup.js
+++ angularModules/questionRelated/questionGroup/directives/Directive-questionGroup.js
@@ -1,45 +1,76 @@
-angular.module('baabtra').directive('questionGroup', function() {
+angular.module('baabtra').directive('questionGroup',['$modal', function($modal) {
 	return {
 		restrict: 'E',
 		require:'ngModel',
 		scope: {
+			ngModel:"="
 		},
 		templateUrl: 'angularModules/questionRelated/questionGroup/directives/Directive-questionGroup.html',
 		link: function(scope, element, attrs, fn) {
 
 		// scope.questionGroup.push(scope.questionModel)
-		    var asd={};
-			scope.questionGroupModel=[asd];
+			scope.questionGroupModel=[];//array to keep the 
+			scope.courseElement={index:1,tlPointInMinute:1000,Name:'Test'};
+			//dummy object created to view the question here
 
+			
 		 scope.dropDown=function (index) {
+		 		scope.index=index;
 		    	var list=[];
 		    //if(!angular.equals(scope.answers[index].value,undefined)){
 		    	
 		    	list.push({text:"Remove",click:function() {
-		    		if(scope.questionGroupModel.length>1){
-			    		scope.questionGroupModel.splice(index,1); //removes that object if there have more than one element in the list
-			    		// if(scope.answers.length==1){
-			    		// 	// this will add an element if there have only one element left after deleting this element
-			    		// 	scope.answers.push({});
-			    		// }
-			    		 //this function rebuilds the key values(numberings) in correct order
+		    		if(scope.questionGroupModel.length>=1){
+			    		scope.questionGroupModel.splice(index,1); //removes that object if 
 		    		}
 		    		}});
 
-
-		    list.push({text:"Insert after",click:function() {
-		    		scope.questionGroupModel.splice(index+1,0,{});
-		    		}});
-
 		    list.push({text:"Insert before",click:function() {
-		    		scope.questionGroupModel.splice(index,0,{});
+		    		scope.position='before';
+		    		scope.QuestionModalOpen();
+		    		// scope.questionGroupModel.splice(index,0,{});
+		    		}});
+		    list.push({text:"Insert after",click:function() {
+		    		scope.position='after';
+		    		scope.QuestionModalOpen();
+		    		// scope.questionGroupModel.splice(index+1,0,{});
 		    		}});
 
 		    	return list;
 		    };
 
-			// question model:{"mark":{"markCriteria":[{}],"totalMark":12},"answer":[{"Name":"b","value":"ans"}],"title":"asd","question":"<div class=\"line\" id=\"line-1\">test</div>","type":"objective","answerType":"singleAnswer","options":[{"Name":"a","value":"asd"},{"Name":"b","value":"ans"},{"Name":"c","value":"qwe"},{"Name":"d","value":""},{"Name":"e"}]}
+
+		    // Pre-fetch an external template populated with a custom scope
+            var questionModal = $modal({scope: scope, template: 'angularModules/questionRelated/questionGroup/directives/Modal-question.html', show: false});
+            // Show when some event occurs (use $promise property to ensure the template has been loaded)
+            scope.QuestionModalOpen = function() {
+              questionModal.$promise.then(questionModal.show);
+            };
+            scope.addQuestion =function(questionModel,index){
+            	if(angular.equals(scope.index,undefined)){
+            	scope.questionGroupModel.push(questionModel);//must pass questionmodel instead of scope.questionmodel
+            	scope.ngModel=scope.questionGroupModel;
+            	}
+            	if(!angular.equals(scope.index,undefined)){
+            		if(!angular.equals(scope.position,'before')){
+		    		 scope.questionGroupModel.splice(scope.index,0,questionModel);
+            		scope.ngModel=scope.questionGroupModel;
+            		}
+            		else if(!angular.equals(scope.position,'after')){
+		    		scope.questionGroupModel.splice(scope.index+1,0,questionModel);
+            		scope.ngModel=scope.questionGroupModel;
+            		}
+            		delete scope.index;//deleted to set the index back to default state
+            	}
+
+            };
+
+            scope.fnFormatObj=function (question) {
+            	return JSON.stringify({value:question});
+            };
+
+			scope.questionModel={"mark":{"markCriteria":[{}],"totalMark":10},"answer":[{"Name":"c","value":"Answer"}],"multiAnswer":true,"question":"<div class=\"line\" id=\"line-1\">This is Testing Question</div>","type":"objective","answerType":"singleAnswer","options":[{"Name":"a","value":"option a"},{"Name":"b","value":"option b"},{"Name":"c","value":"Answer"},{"Name":"d","value":"option d"},{"Name":"e"}]};
 
 		}
 	};
-});
+}]);
