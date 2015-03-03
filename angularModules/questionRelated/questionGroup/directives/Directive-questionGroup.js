5067bbec3372124ac99b68ba62793c74cf1f0fc6
Arun R Menon <arun@baabte.com>
2015-03-03 15:29:25 +0530

    commit to pull the latest

--- angularModules/questionRelated/questionGroup/directives/Directive-questionGroup.js
+++ angularModules/questionRelated/questionGroup/directives/Directive-questionGroup.js
@@ -12,24 +12,31 @@ angular.module('baabtra').directive('questionGroup',['$aside', function($aside) 
 			scope.questionGroupModel=[];//array to keep the 
 			scope.courseElement={index:1,tlPointInMinute:1000,Name:'Test'};
 			//dummy object created to view the question here
-			scope.questionShow="true";
+			scope.questionShow=true;
 			
 		 scope.dropDown=function (index) {
-		 		scope.index=index;
 		    	var list=[];
 		    	
 		    	list.push({text:"Remove",click:function() {
+		    		scope.placeindex=index;
 		    		if(scope.questionGroupModel.length>=1){
 			    		scope.questionGroupModel.splice(index,1); //removes that object if 
+		    			if(angular.equals(index,0)){
+		    				scope.questionShow=true;
+		    			}
 		    		}
 		    		}});
 
 		    list.push({text:"Insert before",click:function() {
+		    		scope.placeindex=index;
 		    		scope.position='before';
+		    		scope.questionShow=true;
 		    		// scope.questionGroupModel.splice(index,0,{});
 		    		}});
 		    list.push({text:"Insert after",click:function() {
+		    		scope.placeindex=index;
 		    		scope.position='after';
+		    		scope.questionShow=true;
 		    		// scope.QuestionModalOpen();
 		    		// scope.questionGroupModel.splice(index+1,0,{});
 		    		}});
@@ -37,34 +44,38 @@ angular.module('baabtra').directive('questionGroup',['$aside', function($aside) 
 		    	return list;
 		    };
 
+		    scope.questionShowActivate =function(){
+		    	scope.questionShow=true;
+		    };
 
 
-          scope.addQuestion =function(questionModel,index){
-            	if(angular.equals(scope.index,undefined)){
+          scope.addQuestion =function(questionModel,placeindex){
+
+            	if(angular.equals(placeindex,undefined)){
             	scope.questionGroupModel.push(questionModel);//must pass questionmodel instead of scope.questionmodel
             	scope.ngModel=scope.questionGroupModel;
             	}
-            	if(!angular.equals(scope.index,undefined)){
+            	if(!angular.equals(placeindex,undefined)){
             		if(!angular.equals(scope.position,'before')){
-		    		 scope.questionGroupModel.splice(scope.index,0,questionModel);
+            			scope.questionGroupModel.splice(placeindex+1,0,questionModel);
             		scope.ngModel=scope.questionGroupModel;
             		}
-            		else if(!angular.equals(scope.position,'after')){
-		    		scope.questionGroupModel.splice(scope.index+1,0,questionModel);
+            		else if(!angular.equals(placeindex,'after')){
+		    			scope.questionGroupModel.splice(placeindex,0,questionModel);
             		scope.ngModel=scope.questionGroupModel;
             		}
-            		delete scope.index;//deleted to set the index back to default state
+            	    scope.placeindex;//deleted to set the index back to default state
             	}
-            	delete scope.question;
+            	delete questionModel;
             	scope.questionShow=false;
-            	scope.questionShow=true;
+            	
 
             };
 
             scope.fnFormatObj=function (question) {
             	return JSON.stringify({value:question});
             };
-            
+
 		}
 
 	};
