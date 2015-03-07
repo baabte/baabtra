16bfaee23ba688d7f829c81740d8b7c5253b0add
vineeth91 <vineeth@baabte.com>
2015-03-07 12:39:29 +0530

    Added partials for Batch addition

--- angularModules/Batches/partials/Partial-manageBatches.js
+++ angularModules/Batches/partials/Partial-manageBatches.js
@@ -1,11 +1,21 @@
 angular.module('baabtra').controller('ManagebatchesCtrl',['$scope','$modal',function($scope,$modal){
- 
-    $scope.fnLoadPopupRepeat=function(mode){
-     //console.log(mode)
-     $modal({scope: $scope, template: 'angularModules/Batches/partials/partial-popupRepeatBatch.html',
-      show: true
-     });
-     
+    
+
+    //$scope.Batch = {};
+    //$scope.Batch.batchMode = "onetime";
+    $scope.fnLoadPopupRepeat=function(batchMode){
+      console.log(batchMode);
+      if(batchMode=="repeat"){
+         $modal({scope: $scope, template: 'angularModules/Batches/partials/partial-popupRepeatBatch.html',
+          show: true
+         });
+      }else{
+          $modal({scope: $scope, template: 'angularModules/Batches/partials/partial-popuponeTimeBatch.html',
+          show: true
+         });
+      }
+
+      
     }
    $scope.repeatArrays=[{id:0,text:'Daily'},{id:1,text:'Weekly'},{id:2,text:'Monthly'},{id:3,text:'Yearly'}];  
    $scope.repeatEvery=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
