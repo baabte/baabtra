7fd258aa75c7d0e5f4b5c54b1a019038d8fb3607
Arun R Menon <arun@baabte.com>
2015-03-12 11:53:27 +0530

    commit with changes in feedback viewer( answer will be binding back if loaded) by arun

--- angularModules/feedback/feedbackViewer/directives/Directive-feedbackViewer.js
+++ angularModules/feedback/feedbackViewer/directives/Directive-feedbackViewer.js
@@ -4,18 +4,20 @@ angular.module('baabtra').directive('feedbackViewer',['$rootScope','bbConfig','$
 		replace: true,
 		scope: {
 			feedbackForm:'=',
-			feedbackResponse:'='
+			feedbackResponse:'=',
+			userResponse:'='
 		},
 		templateUrl: 'angularModules/feedback/feedbackViewer/directives/Directive-feedbackViewer.html',
-		link: function(scope, element, attrs, fn) {
+		link: function(scope, element, attrs, fn){
 
 		
 		//creating the array to keep the user response 
-		for(var index in scope.feedbackForm){
-			var tempObj={userResponse:[]};
-			scope.feedbackResponse.push(tempObj);
-		};
 		
+			for(var index in scope.feedbackForm){
+				var tempObj={userResponse:[]};
+				scope.feedbackResponse.push(tempObj);
+			}
+
 		scope.markScored=0;//mark is not used here
 
 		scope.mark=JSON.stringify({totalMark:0}); //markobject set as 0
