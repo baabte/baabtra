05088d51c7e6ed31f523815bcdda8d0e99aa8f56
jihin <jihin@baabte.com>
2015-03-17 15:42:01 +0530

    roleUserLoader by Jihin

--- angularModules/common/directives/Directive-roleUserLoader.js
+++ angularModules/common/directives/Directive-roleUserLoader.js
@@ -13,7 +13,7 @@ angular.module('baabtra').directive('roleUserLoader', ['$rootScope', 'commonSrv'
 			if($rootScope.userinfo.ActiveUserData.roleMappingObj.fkCompanyId){
 			  companyId = $rootScope.userinfo.ActiveUserData.roleMappingObj.fkCompanyId.$oid;				
 			}
-			var roleUserResponse = commonSrv.fnLoadUsersUnderRole(scope.roleId,companyId);
+			var roleUserResponse = commonSrv.fnLoadUsersUnderRole("54e1780103e38dc70f5efb96",companyId);
 			roleUserResponse.then(function(response){
 				var usersUnderRoles = angular.fromJson(JSON.parse(response.data));
 				angular.forEach(usersUnderRoles, function(user){
@@ -25,7 +25,9 @@ angular.module('baabtra').directive('roleUserLoader', ['$rootScope', 'commonSrv'
 			});
 
 			scope.onItemClick = function(item){
-				
+				if(angular.equals(scope.ngModel,undefined)){
+					scope.ngModel = [];
+				}
 				scope.ngModel.push({ticked:item.ticked,roleMappingId:item.roleMappingId,Name:item.Name});
 			};
 		}
