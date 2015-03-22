b46baac45f712c9d1247c5fa7ee7d5f948a80804
akshath4u <akshath@baabte.com>
2015-03-22 20:47:34 +0530

    commited batch attendance module

---
+++ angularModules/Batches/directives/Directive-attendanceMenteeList.js
@@ -0,0 +1,40 @@
+angular.module('baabtra').directive('attendanceMenteeList',['attendenceService', function(attendenceService) {
+	return {
+		restrict: 'E',
+		replace: true,
+		required:"ngModel",
+		scope: {
+			exclusionList:"=",
+			timeLineObj:"="
+		},
+		templateUrl: 'angularModules/Batches/directives/Directive-attendanceMenteeList.html',
+		link: function(scope, element, attrs, fn) {
+			scope.$watch('timeLineObj',function(){
+				console.log(scope.timeLineObj);
+				if(angular.equals(scope.timeLineObj.courseElement.excludeList,undefined)){
+					scope.timeLineObj.courseElement.excludeList=[];
+				}
+			});
+
+			
+			//marking attendence function will triger on click
+			scope.fnMarkAttendence=function(user){	
+				//console.log(courseElement.courseElement.attendence);
+				console.log(scope.timeLineObj);
+				var markAttendencePromise=attendenceService.markAttendence(user._id.$oid,scope.timeLineObj.tlpoint,scope.timeLineObj.userCourseElementType,scope.timeLineObj.innerIndex,scope.timeLineObj.courseElement.attendence);
+				markAttendencePromise.then(function(data){
+			    var result=angular.fromJson(JSON.parse(data.data));
+				
+
+				});
+			};
+			//converting back to day
+			scope.changeminutes2day=function(minutes) {
+				var day= Math.ceil((minutes/60)/24);
+				return day;
+			};
+
+			
+		}
+	};
+}]);
--- angularModules/Batches/partials/Partial-batchAttendance.js
+++ angularModules/Batches/partials/Partial-batchAttendance.js
@@ -1,4 +1,52 @@
-angular.module('baabtra').controller('BatchattendanceCtrl',function($scope){
+angular.module('baabtra').controller('BatchattendanceCtrl',['$scope','$rootScope','viewBatches','$stateParams','$alert',function($scope,$rootScope,viewBatches,$stateParams,$alert){
+
+	$scope.batchObj={}; //mainObject
+	$rootScope.$watch('userinfo',function(){ //watch for getting the basic details from rootscope
+		$scope.loggedusercrmid = $rootScope.userinfo.ActiveUserData.roleMappingId.$oid;
+		$scope.companyId=$rootScope.userinfo.ActiveUserData.roleMappingObj.fkCompanyId.$oid;
+		$scope.batchMappingId=$stateParams.batchMappingId;
+		loadCourseMaterialsDDl=viewBatches.loadCourseMaterials4batchAtt($scope);
+
+		loadCourseMaterialsDDl.then(function(response){ //promise for batch load
+		
+			var outcomeObj=angular.fromJson(JSON.parse(response.data))
+			if(!angular.equals(outcomeObj.result,'notfound')){
+					$scope.batchObj.batchDetails=outcomeObj.courseBatchObj;
+					$scope.batchObj.materialList=outcomeObj.userCourseElementlist;
+					$scope.status=true;
+			}
+			else{
+					$scope.batchObj.batchDetails=outcomeObj.courseBatchObj;
+
+					$scope.status=false;
+			}
+		});
+	});
+
+	//
+	$scope.$watch('batchObj.selectedCourseMaterial',function(){
+		/*for(var key in batchObj.selectedCourseMaterial){
+			$scope.batchObj.selectedCourseMaterial[key];
 
 
-});
+			loadMenteesPromise=viewBatches.loadMentees4batchAtt($scope,$scope.batchObj.selectedCourseMaterial[key]);
+
+			loadMenteesPromise.then(function(response){ //promise for batch load
+			
+				var outcomeObj=angular.fromJson(JSON.parse(response.data))
+				if(!angular.equals(outcomeObj.result,'notfound')){
+						//$scope.batchObj.batchDetails=outcomeObj.courseBatchObj;
+						//$scope.batchObj.materialList=outcomeObj.userCourseElementlist;
+						//$scope.status=true;
+				}
+				else{
+						//$scope.batchObj.batchDetails=outcomeObj.courseBatchObj;
+
+						//$scope.status=false;
+				}
+			});
+
+		}*/
+	});
+
+}]);
