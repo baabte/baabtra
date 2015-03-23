670249c05f54979c5d6682fdbaf7f2bfe2a21188
akshath4u <akshath@baabte.com>
2015-03-23 12:31:55 +0530

    commited batch attendance design fixes

--- angularModules/common/directives/Directive-setParentHeight.js
+++ angularModules/common/directives/Directive-setParentHeight.js
@@ -1,9 +1,21 @@
 angular.module('baabtra').directive('setParentHeight', function() {
 	return {
 		restrict: 'A',
+		required:"ngModel",
+		scope:{
+			clName:"="
+		},
+		
 		link: function(scope, element, attrs, fn) {
-
-			var height=$(element).parents().find('.set-full-height').height();
+			var height;
+			if(!angular.equals(scope.clName,undefined)){
+				
+				height=$(element).parents().find('.'+scope.clName).height();
+			}
+			else{
+				height=$(element).parents().find('.set-full-height').height();
+			}
+			alert(height);
 			$(element).height(height);
 		}
 	};
