7b713c248c929d4c675d7ecc616515ee20358e46
vineeth91 <vineeth@baabte.com>
2015-03-23 12:13:02 +0530

    created service for sending mails

--- angularModules/user/services/Service-userRegistrationService.js
+++ angularModules/user/services/Service-userRegistrationService.js
@@ -24,15 +24,11 @@ this.FnRegisterUser=function(userRegister){
       return promise;
    };
 
-this.fnSendEmailSmsNotification=function(phone,email,senderId,smskey){
-  console.log(smskey);
-  console.log(phone);
-  console.log(email);
-  console.log(senderId);
+this.fnSendEmailSmsNotification=function(userEmail,userName,menteeEmail,menteeName,evaluatorEmailLIst,companyName,companyLogo){
   var promise = $http({
     method: 'POST',
       url: bbConfig.BWS+'sendEmailSmsNotification/',
-      data:{"phone":phone,"email":email,"senderId":senderId,"smsKey":smskey}
+      data:{"userEmail":userEmail,"userName":userName,"menteeEmail":menteeEmail,"menteeName":menteeName,"evaluatorEmailLIst":evaluatorEmailLIst,"companyName":companyName,"companyLogo":companyLogo}
    });
   return promise;
  }
