2bee5148bdbe6ed18b061b610babf874712383a6
akshath4u <akshath@baabte.com>
2015-03-25 18:09:46 +0530

    commited design chnges in element preview

---
+++ angularModules/common/directives/Directive-materialPreview.js
@@ -0,0 +1,21 @@
+angular.module('baabtra').directive('materialPreview',['$modal', function($modal) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			data:"="
+		},
+		templateUrl: 'angularModules/common/directives/Directive-materialPreview.html',
+		link: function(scope, element, attrs, fn) {
+			console.log(scope.data);
+			scope.elementPreview=function(){
+				
+            	// Show when some event occurs (use $promise property to ensure the template has been loaded)
+            	var modalPreview = $modal({scope: scope, template: 'angularModules/common/popup/popup-elementPreview.html', show: false});
+             	modalPreview.$promise.then(modalPreview.show);
+           
+			};
+
+		}
+	};
+}]);
