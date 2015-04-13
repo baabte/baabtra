2ea90adaa51b14dcd74bf8248054c806ea937d66
Anoop <anoop@baabte.com>
2015-04-13 13:23:06 +0530

    orderform approval flow mid-way commit

--- angularModules/user/partials/Partial-viewUsersForApprove.js
+++ angularModules/user/partials/Partial-viewUsersForApprove.js
@@ -25,8 +25,7 @@ angular.module('baabtra').controller('ViewusersforapproveCtrl',['$scope', '$root
 	
 	$scope.data.statusTypes = [{"value":"requested","label":"Requested"},
 							   {"value":"approved","label":"Approved"},
-							   {"value":"rejected","label":"Rejected"}];
-	$scope.data.selectedStatusTypes = "requested";
+							   {"value":"rejected","label":"Rejected"}];	
 	$scope.data.approveOrderFormStatus = false;
 	$scope.viewOrderForm = function(orderForm){
 		$scope.data.orderForm = orderForm;
@@ -47,56 +46,32 @@ angular.module('baabtra').controller('ViewusersforapproveCtrl',['$scope', '$root
 //ANOOP ***************************************************************************************
 	//creating a mock of the global configuration of the company for the approval flow and access privileges for roles
 
-	$scope.approvalFlow = [{displayName:"Verify Applicants", loadStatus:["Pending Approval"], nextStatus:"Verified", privilegedRoles:['a','b','c'], buttonText:"Verify", paymentStage:false },
-	{displayName:"Collect Payment", loadStatus:["Verified", "Partially Paid"], nextStatus:"Paid", privilegedRoles:['a','b'], buttonText:"Make Payment", paymentStage:true },
-	{displayName:"Approve Order Form", loadStatus:["Paid"], nextStatus:"Approved", privilegedRoles:['a','c'], buttonText:"Approve", paymentStage:false}
+	$scope.approvalFlow = [{currentStage:'Verification',displayName:"Verify Applicants", loadStatus:["Pending Approval"], nextStatus:"Verified", privilegedRoles:['a','b','c'], buttonText:"Verify", paymentStage:false },
+	{currentStage:'Payment',displayName:"Collect Payment", loadStatus:["Verified", "Partially Paid"], nextStatus:"Paid", privilegedRoles:['a','b'], buttonText:"Make Payment", paymentStage:true },
+	{currentStage:'Approval',displayName:"Approve Order Form", loadStatus:["Paid"], nextStatus:"Approved", privilegedRoles:['a','c'], buttonText:"Approve", paymentStage:false}
 	];	
 
 	//.End == creating a mock of the global configuration of the company for the approval flow and access privileges for roles
+
 
 	//taking the role of the current logged in user to a variable
 	//$scope.currentUserRole = angular.copy($rootScope.userinfo.ActiveUserData.roleMappingObj.fkRoleId);
 	$scope.currentUserRole = 'a';
 
+	//setting a current stage, this must be taken from the url since every stage will be having a menu link
+	var currentStageIndex = 1;	
+	$scope.currentStage = {};
+	$scope.currentStage = $scope.approvalFlow[currentStageIndex];
 
-	//creating a function to bind custom action dropdown to an order form (for eg. if the order form is already verified, then the verify option should not appear on the respective order form)
-	$scope.data.activeOptions=[];
-	$scope.getOptions = function(orderFormStatus, orderFormIndex){
-
-
-		//creating an array for the active options to get the clicked element on the corresponding order form
-		$scope.data.activeOptions[orderFormIndex]=[];
-
-			//Creating the options for each role dynamically to show in the options menu, for eg. if the logged in user is of role 'a' only the entries which has role 'a' in their privilegedroles array will appear in the menu
-			$scope.data.orderFormOptions = [];
-
-			//a variable to keep the active options for the user so that for the click on an option the properties of the particular object in the approval flow object can be tracked
-			
-			var orderFormOption = {};
-			for (var i in $scope.approvalFlow) {				
-				//checking for the uder's previlege to change the status
-				if(!angular.equals($scope.approvalFlow[i].privilegedRoles.indexOf($scope.currentUserRole),-1)){	
-
-
-				// if the order form is in a status which is not in the load status of the current step in the approval flow, that link should not be shown, for eg, if an order form can be set to approved state from verified or paid state, then only the order forms in those statuses should be shown		
-
-					//if the status of the order form is already the next status of the approval form the option to change to that status should not be shown
-						 if(!angular.equals(orderFormStatus.toLowerCase(), $scope.approvalFlow[i].nextStatus.toLowerCase() ) && !angular.equals($scope.approvalFlow[i].loadStatus.indexOf(orderFormStatus), -1) ) {
-
-						 	orderFormOption = {text: $scope.approvalFlow[i].displayName, click: "this.approveOrderForm(orderForm," + orderFormIndex + ", $index)"};
-							if(!angular.equals(orderFormOption.text, undefined) && !angular.equals(orderFormOption.text, "")){
-
-								$scope.data.orderFormOptions.push(orderFormOption);
-								$scope.data.activeOptions[orderFormIndex].push($scope.approvalFlow[i]);
-							}
-
-					}
-				}
-			};
-			
-			return $scope.data.orderFormOptions;
+	// checking whether the current role has access to change the status
+	if(angular.equals($scope.currentStage.privilegedRoles.indexOf($scope.currentUserRole), -1)) {
+		$state.go('home.main');
 	}
-	// **End**
+
+// set an array that contains the status of which the order forms are to be loaded
+
+$scope.data.selectedStatusTypes = $scope.currentStage.loadStatus;
+
 
 	//show or hide order form by looking at its tatus and accessibility by the logged in user
 	$scope.showOrderFormOnStatus = function(orderFormStatus){
@@ -125,15 +100,19 @@ angular.module('baabtra').controller('ViewusersforapproveCtrl',['$scope', '$root
 
 	//**End**
 
-	//function to trigger when an option from the options menu of each order form is clicked
-		$scope.approveOrderForm = function(orderForm,orderFormIndex, index){
 
-		
-		//getting the currently clicked object to an object in the current scope
-		$scope.currentClickedObject = $scope.data.activeOptions[orderFormIndex][index];
+
+
+	//function to trigger when an option from the options menu of each order form is clicked
+		$scope.approveOrderForm = function(orderForm){
 		
 		//getting the order from to an object in scope
 		$scope.data.approveOrderForm = orderForm;
+
+		//taking the current orderform into a variable
+		$scope.currentOrderForm = orderForm;
+
+		$scope.createCurrencyArray();
 
 		 // Pre-fetch an external template populated with a custom scope
             var myOtherModal = $modal({scope: $scope, template: 'angularModules/Nomination/partials/popup-orderFormApprovalFlow.html', show: true});
@@ -153,7 +132,7 @@ angular.module('baabtra').controller('ViewusersforapproveCtrl',['$scope', '$root
 //function to show or hide requests on the basis of the status of the request
 $scope.showHideIndividualRequestForApproval = function(requestStatus){
 
-	if(!angular.equals($scope.currentClickedObject.loadStatus.indexOf(requestStatus),-1)){
+	if(!angular.equals($scope.currentStage.loadStatus.indexOf(requestStatus),-1)){
 
 		return true;
 	}
@@ -187,13 +166,12 @@ $scope.checkRequestsForStatus = function(course, statusArray){
 	return false;
 }
 
-$scope.setStatus = [];
 
 //creating a function to update the status of a mentee when the user checks a checkbox
 $scope.updateStatus = function (mentee, checked){
 
 	if(checked){	
-		mentee.statusTobeChangedTo = $scope.currentClickedObject.nextStatus;
+		mentee.statusTobeChangedTo = $scope.currentStage.nextStatus;
 	}
 	else {
 		delete mentee.statusTobeChangedTo;
@@ -218,10 +196,32 @@ $scope.checkAll = function(checked){
 	
 }
 
+//get the array of currencies in the courses to show the payment screen, this is needed since each course may be configured to have a differnet currency
+$scope.createCurrencyArray = function() {
+
+		if($scope.currentStage.paymentStage){
+			$scope.currencyArray = [];
+
+			//looping through the current order form to create the currency array
+			for(var i in $scope.currentOrderForm.orderDetails){
+
+				var currentCourse = $scope.currentOrderForm.orderDetails[i];
+
+				if(angular.equals($scope.currencyArray.indexOf(currentCourse.currency),-1)){
+					$scope.currencyArray.push({currency:currentCourse.currency});
+				}
+
+			}		
+
+		}
+}
+
+
 //function to update the orderform status
 $scope.updateOrderFormStatus = function(){
 
-	console.clear();
+	// A variable to hold tha data that at least one entry has been selected for status change
+	var minimumSelection = false;
 
 	// creating a copy of the original orderForm to hold the updated values, this is done like this to prevent a false notification to the user before the actual database update gets carried out
 	var updatedOrderForm = angular.copy($scope.data.approveOrderForm);
@@ -241,6 +241,8 @@ $scope.updateOrderFormStatus = function(){
 			
 			if(!angular.equals(request.statusTobeChangedTo, undefined)){
 				
+				//set the variable that at least one entry has been selected
+				minimumSelection = true;
 
 				// pushing the current status into a status history array
 				if(angular.equals(request.statusHistory, undefined)){
@@ -263,14 +265,14 @@ $scope.updateOrderFormStatus = function(){
 
 			}
 			else{
-				if(!angular.equals(request.status, $scope.currentClickedObject.nextStatus)){
+				if(!angular.equals(request.status, $scope.currentStage.nextStatus)){
 
 					changeOrderFormStatus = false;
 				}
 			}
 
 		// if the stage is a payment stage set the data for the same
-			if($scope.currentClickedObject.paymentStage){
+			if($scope.currentStage.paymentStage){
 
 				if(angular.equals(actTransactions, undefined)){
 					var actTransactions = [];			
@@ -279,7 +281,16 @@ $scope.updateOrderFormStatus = function(){
 
 			}
 
+			delete request.checkedStatus;
+
 		}
+	}
+
+
+	// exiting the function if no entry has been selected for status change
+	if(!minimumSelection){
+			$alert({title:'Operation Aborted. ',content:'Please select at least one entry to ' + $scope.currentStage.buttonText, placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'warning', show:true});
+			return false;
 	}
 
 	//change the status of the order form if the status of all the requests in the orderfrom has been changed
@@ -291,7 +302,7 @@ $scope.updateOrderFormStatus = function(){
 		}
 
 		orderFormStatusHistory.previousStatus = updatedOrderForm.status;
-		orderFormStatusHistory.statusChangedTo = $scope.currentClickedObject.nextStatus;
+		orderFormStatusHistory.statusChangedTo = $scope.currentStage.nextStatus;
 	
 		orderFormStatusHistory.statusChangedOn = d.toISOString();
 		orderFormStatusHistory.statusChangedby = $scope.rmId;
@@ -299,7 +310,7 @@ $scope.updateOrderFormStatus = function(){
 
 		updatedOrderForm.statusHistory.push(orderFormStatusHistory);
 		
-		updatedOrderForm.status =$scope.currentClickedObject.nextStatus; 
+		updatedOrderForm.status =$scope.currentStage.nextStatus; 
 
 	}
 
@@ -309,8 +320,9 @@ $scope.updateOrderFormStatus = function(){
 	updatedOrderForm.companyId = updatedOrderForm.companyId.$oid;
 	updatedOrderForm.urmId = updatedOrderForm.urmId.$oid;
 	updatedOrderForm.crmId = updatedOrderForm.crmId.$oid;	
-	updatedOrderForm.createdDate = $filter('date')(updatedOrderForm.createdDate.$date);	
-
+	updatedOrderForm.createdDate = new Date($filter('date')(updatedOrderForm.createdDate.$date));	
+    
+	console.log(updatedOrderForm.createdDate);
 	var updateOrderForm = nomination.fnUpdateOrderFormStatus(updatedOrderForm);
 
 	updateOrderForm.then(function(response){
@@ -320,11 +332,11 @@ $scope.updateOrderFormStatus = function(){
 		if(angular.equals(result, 'success')){
 			$scope.data.approveOrderForm = updatedOrderForm;
 			delete updatedOrderForm;
-			$alert({title:'Done. ',content:'The statuses have been set to ' + $scope.currentClickedObject.nextStatus + ' successfully.', placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'success', show:true});
+			$alert({title:'Done. ',content:'The statuses have been set to ' + $scope.currentStage.nextStatus + ' successfully.', placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'success', show:true});
 		}
 		else{
 			delete updatedOrderForm;
-			$alert({title:'Error. ',content:'We could not update the statuses this time, please click on the "' + $scope.currentClickedObject.buttonText + '" button to retry.', placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'danger', show:true});
+			$alert({title:'Error. ',content:'We could not update the statuses this time, please click on the "' + $scope.currentStage.buttonText + '" button to retry.', placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'danger', show:true});
 		}
 
 	});
@@ -335,7 +347,7 @@ $scope.updateOrderFormStatus = function(){
 
 // ======================================================================================
 //ANOOP ***************************************************************************************
-
+	console.log($scope.data.selectedStatusTypes);
 
 	var LoadMenteesResponse = viewUsersForApprove.fnLoadMenteesForApprove($scope.cmpId, $scope.data.selectedStatusTypes);
 	LoadMenteesResponse.then(function(response){
