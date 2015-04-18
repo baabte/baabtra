3b67b0ffc076114b4faf8748b50d360cef1d3451
jihin <jihin@baabte.com>
2015-04-18 19:45:42 +0530

    nomination by Jihin

--- angularModules/Nomination/partials/Partial-nomination.js
+++ angularModules/Nomination/partials/Partial-nomination.js
@@ -142,7 +142,6 @@ $scope.requesteDetailsCompleted = function(){
 $scope.checkUserAlreadyExists = function(){
 	
 	if(!angular.equals($scope.data.requesteeDetails.eMail,undefined)){
-		console.log($scope.data.requesteeDetails.eMail);
 		var companyCustomerDetailsResponse = nomination.fnLoadCompanyCustomerDetails($scope.data.requesteeDetails.eMail, companyId, $scope.data.requesteeDetails.type);
 		companyCustomerDetailsResponse.then(function(response){
 			console.log(angular.fromJson(JSON.parse(response.data)));
@@ -194,7 +193,7 @@ $scope.fnUserRegister =function () {
 		var courseImageUploadResponse = commonSrv.fnFileUpload($scope.allSync.FormData[filePaths[index]],filePaths[index]);
   				courseImageUploadResponse.then(function(response){
   				var imagePath = response.data.replace('"','').replace('"','');
-          		$scope.allSync.FormData[filePaths[$scope.fileUpload]] = bbConfig.BWS + 'files/'+ filePaths[index] +'/' + imagePath;
+          		$scope.allSync.FormData[filePaths[$scope.fileUpload]] = bbConfig.BWS + 'files/'+ filePaths[$scope.fileUpload] +'/' + imagePath;
           		$scope.fileUpload ++;
         	});
 		}
