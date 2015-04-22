03c814fbf088f6491b3c0fe1e643058c244d76f0
Arun R Menon <arun@baabte.com>
2015-04-22 09:49:35 +0530

    commit with latest changes in course allocate

--- angularModules/user/partials/Partial-courseAllocate.js
+++ angularModules/user/partials/Partial-courseAllocate.js
@@ -1,6 +1,6 @@
-angular.module('baabtra').controller('CourseallocateCtrl',['$scope','bbConfig','$rootScope','$state','commonService','$alert',function($scope,bbConfig,$rootScope,$state,commonService,$alert){
+angular.module('baabtra').controller('CourseallocateCtrl',['$scope','bbConfig','$rootScope','$state','commonService','courseAllocateService','$alert',function($scope,bbConfig,$rootScope,$state,commonService,courseAllocateService,$alert){
 
-	if(!$rootScope.userinfo){
+if(!$rootScope.userinfo){
    commonService.GetUserCredentials($scope);
    $rootScope.hide_when_root_empty=false;
 }
@@ -9,8 +9,49 @@ if($rootScope.loggedIn===false){
  $state.go('login');
 }
 
+$rootScope.$watch('userinfo',function(){
+    $scope.loggedusercrmid = $rootScope.userinfo.ActiveUserData.roleMappingId.$oid;
+    $scope.companyId=$rootScope.userinfo.ActiveUserData.roleMappingObj.fkCompanyId.$oid;
 
-$scope.selectedCourse={};
+    var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,'','initial',''); 
+    fetchUsersToCourseAllocateCallback.then(function(data){
+        $scope.userObj=angular.fromJson(JSON.parse(data.data));
+        console.log($scope.userObj);
+        $scope.firstUser=$scope.userObj.firstId.$oid;
+    });
+});
+
+
+$scope.courseAllocate={};
+
+$scope.courseAllocate.selectedCourse={};
+
+$scope.prevButtondisabled=true;
+
+$scope.nextOne=function(){//event  for showing next 12 items
+	  $scope.prevButtondisabled=false;
+	   var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,$scope.userObj.firstId.$oid,'next',$scope.userObj.lastId.$oid);
+	   fetchUsersToCourseAllocateCallback.then(function(data){
+        $scope.userObj=angular.fromJson(JSON.parse(data.data));
+       });
+};
+
+$scope.prevOne=function(){//event  for showing previous 9 items
+	  if (angular.equals($scope.firstUser,$scope.userObj.firstId.$oid)){ 
+		$scope.prevButtondisabled=true;
+	  }
+	  else{
+	   var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,$scope.userObj.firstId.$oid,'prev',$scope.userObj.lastId.$oid);
+	   fetchUsersToCourseAllocateCallback.then(function(data){
+	        $scope.userObj=angular.fromJson(JSON.parse(data.data));
+	         if (angular.equals($scope.firstUser,$scope.userObj.firstId.$oid)){ 
+				$scope.prevButtondisabled=true;
+	  		}
+	   });
+   	  }
+  
+};
+
 
 
 }]);
--- angularModules/user/services/Service-courseAllocateService.js
+++ angularModules/user/services/Service-courseAllocateService.js
@@ -1,6 +1,15 @@
-angular.module('baabtra').factory('courseAllocateService',function() {
+angular.module('baabtra').service('courseAllocateService',['$http','bbConfig',function($http,bbConfig) {
 
-	var courseAllocateService = {};
 
-	return courseAllocateService;
-});
+	 this.fnfetchUsersToCourseAllocate=function($scope,firstId,type,lastId){
+    var promise = $http({
+          method: 'POST',
+          url: bbConfig.BWS+'FetchUsersToCourseAllocate/',
+          data:{"companyId":$scope.companyId,"firstId":firstId,"type":type,"lastId":lastId},
+          contentType:'application/json; charset=UTF-8',
+        })
+
+        return promise;
+      };
+
+}]);
