cca7892c54bf32c1b8982f26e137b9608d822740
Arun R Menon <arun@baabte.com>
2015-04-23 10:02:36 +0530

    commit to pull the latest

--- angularModules/user/partials/Partial-courseAllocate.js
+++ angularModules/user/partials/Partial-courseAllocate.js
@@ -25,31 +25,50 @@ $rootScope.$watch('userinfo',function(){
 $scope.courseAllocate={};
 
 $scope.courseAllocate.selectedCourse={};
+$scope.courseAllocate.selectedUsers={};
 
 $scope.prevButtondisabled=true;
 
 $scope.nextOne=function(){//event  for showing next 12 items
 	  $scope.prevButtondisabled=false;
-	   var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,$scope.userObj.firstId.$oid,'next',$scope.userObj.lastId.$oid);
+	   var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,$scope.userObj.firstId,'next',$scope.userObj.lastId);
 	   fetchUsersToCourseAllocateCallback.then(function(data){
         $scope.userObj=angular.fromJson(JSON.parse(data.data));
+        console.log($scope.userObj)
        });
 };
 
-$scope.prevOne=function(){//event  for showing previous 9 items
-	  if (angular.equals($scope.firstUser,$scope.userObj.firstId.$oid)){ 
+//event  for showing previous 9 items
+$scope.prevOne=function(){
+	  
+	  if(angular.equals($scope.firstUser,$scope.userObj.firstId)){ 
 		$scope.prevButtondisabled=true;
 	  }
 	  else{
-	   var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,$scope.userObj.firstId.$oid,'prev',$scope.userObj.lastId.$oid);
+	   var fetchUsersToCourseAllocateCallback=courseAllocateService.fnfetchUsersToCourseAllocate($scope,$scope.userObj.firstId,'prev',$scope.userObj.lastId);
 	   fetchUsersToCourseAllocateCallback.then(function(data){
 	        $scope.userObj=angular.fromJson(JSON.parse(data.data));
-	         if (angular.equals($scope.firstUser,$scope.userObj.firstId.$oid)){ 
+        	console.log($scope.userObj)
+	         if (angular.equals($scope.firstUser,$scope.userObj.firstId)){ 
 				$scope.prevButtondisabled=true;
 	  		}
 	   });
    	  }
-  
+};
+
+$scope.fnUserSelection=function(userobj,index){
+	user=angular.copy(userobj);
+	if(angular.equals($scope.courseAllocate.selectedUsers[user.userRoleMappingId],undefined)){
+	$scope.courseAllocate.selectedUsers[user.userRoleMappingId]=user;
+	 $scope.userObj.userList.splice(index,1);
+	console.log($scope.courseAllocate.selectedUsers);
+	}
+	else{
+	 $scope.userObj.userList.splice(index,1,user);
+	 delete $scope.courseAllocate.selectedUsers[user.userRoleMappingId];
+	console.log($scope.courseAllocate.selectedUsers);
+	}
+
 };
 
 
