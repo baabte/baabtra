aabb76121acb47f6f7fa5dfc794fec7f4d07d310
Jihin <jihin@baabte.com>
2015-04-26 22:47:17 +0530

    single question evaluation by Jihin

--- config.js
+++ config.js
@@ -62,9 +62,8 @@ angular.module('baabtra')
    $linkedInProvider.set('appKey', '78jnfwsxzeqtdl').set('scope', "r_fullprofile r_network r_emailaddress").set('authorize', true);
   }])
   .constant('bbConfig',{ //used for storing enviornment variables
-      "BWS": "http://127.0.0.1:8000/",//local
-
-     // "BWS": "http://services.baabtra.com/",//server - production
+     // "BWS": "http://127.0.0.1:8000/",//local
+     "BWS": "http://services.baabtra.com/",//server - production
      //"BWS": "http://server.mb-test.in/",//server-test
      "SARID":1, // SuperAdminRoleID
      "CURID":2, // CompanyUser
@@ -74,7 +73,7 @@ angular.module('baabtra')
 
 angular.module('angular-custom-form',[]) /*Config constant for angular custom form */
 .constant('bbConfig',{ //used for storing enviornment variables
-     "BWS": "http://127.0.0.1:8000/",//local
+     // "BWS": "http://127.0.0.1:8000/",//local
       // "BWS": "http://services.baabtra.com/",//server - production
     // "BWS": "http://server.mb-test.in/",//server-test
      "SARID":1, // SuperAdminRoleID
