8680ee8d311e90e1499b6578b21cef1a5208a9e7
Anoop <anoop@baabte.com>
2015-04-28 16:30:57 +0530

    added the assignment viewer and assignment evaluator

--- angularModules/courseElementFields/assignmentViewer/directives/Directive-assignmentViewer.js
+++ angularModules/courseElementFields/assignmentViewer/directives/Directive-assignmentViewer.js
@@ -1,4 +1,4 @@
-angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state','assignmentFunctions',function($rootScope, $state, assignmentFunctions) {
+angular.module('baabtra').directive('assignmentViewer',['$rootScope','$state','assignmentFunctions','$alert',function($rootScope, $state, assignmentFunctions,$alert) {
 	return {
 		restrict: 'E',
 		replace: true,
@@ -15,11 +15,14 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
 		var elementArray = angular.copy(scope.$parent.previewData.elements);
 		
 		
-		var currentElement;		
-		for (var i in elementArray)	{
-			 currentElement = elementArray[i];
+		var currentElement;
+		//looping in the original parent object to calculate the marks, any change in the parent scope will force the respective directive to load once more as it will trigger a watch on the parent scope. That is the reason why we are taking a copy of the parnet scope in the line above. But here we want the watch to trigger as it has to modify a custom property on the question directive which will hide the submit button.		
+		for (var i in scope.$parent.previewData.elements)	{
+			 currentElement = scope.$parent.previewData.elements[i];
 			 if(!angular.equals(currentElement,null)){
 			 	if(angular.equals(currentElement.type, 'question-viewer')){
+
+
 			 	
 			 		// create a custom attributes array if it does not exist
 			 		if(angular.equals(currentElement.customAttributes, undefined)){
@@ -29,6 +32,9 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
 			 		if(angular.equals(currentElement.customAttributes['show-submit-button'],undefined)){
 			 			currentElement.customAttributes['show-submit-button'] ='false';
 			 		}
+
+			 		console.clear();
+			 		console.log(currentElement.customAttributes);
 
 			 		//get the total marks for the assignment
 			 	    if (angular.equals(scope.totalMarks, undefined)){
@@ -47,27 +53,27 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
   // function to save the answer of the assignment
   scope.submitAssignment = function(submitStatus) {
 
-
-
-  	//an array to save the answer objects
-  	var answerArray = [];
+  	
   	var totalMarksScored = 0;
+
   	for (var i in elementArray)	{
 			 currentElement = elementArray[i];
 			 if(!angular.equals(currentElement,null)){
 			 	
 			 	if(angular.equals(currentElement.type, 'question-viewer')){			 		
-			 	
-			 		scope.$parent.childElementScopes[i].saveAnswer(function (argument) {		 			
-			 			//changing the submit status of the answer
-			 			argument.answer.submitStatus = submitStatus;			 			
+			 		//calling the save answer function in the question viewer directive, but here we want the submit status to be saved as draft or whatever status which is sent from here, so if that function is passed with an argument, the function will replce those keys with the values passed from here.
+			 		var evStatus = 2;
+			 		if(angular.equals(submitStatus,"submitted")){
+			 			evStatus = 0;
+			 		}
+			 		scope.$parent.childElementScopes[i].saveAnswer({submitStatus:submitStatus,
+			 			evaluated:evStatus});
 
-			 			answerArray.push(argument);
-			 			
-			 		})
 
-			 		totalMarksScored = parseInt(totalMarksScored) + parseInt(currentElement.value.markScored);
+			 		if(!angular.equals(currentElement.value.markScored, undefined)){
 
+			 			totalMarksScored = parseInt(totalMarksScored) + parseInt(currentElement.value.markScored);
+			 		}
 			 		
 			 	}
 			 }
@@ -77,11 +83,10 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
 
 	//check for and apply penalties
 	if(angular.equals(submitStatus,'submitted')) {		
-		if(checkAndApplyPenalty){			
-			findPenalty();
-			if(penaltyHistory.length > 0){
-
-				totalMarksScored = applyPenalty(totalMarksScored);
+		if(scope.checkAndApplyPenalty){			
+			assignmentFunctions.findPenalty(scope);
+			if(scope.penaltyHistory.length > 0){
+				totalMarksScored = assignmentFunctions.applyPenalty(scope,  totalMarksScored);
 			}
 
 					
@@ -96,7 +101,7 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
      objToBeSaved.lastUpdatedBy = $rootScope.userinfo.ActiveUserData.roleMappingObj._id;
      objToBeSaved.markScored = totalMarksScored;
      objToBeSaved.status = submitStatus;
-     objToBeSaved.answerArray = answerArray
+     //objToBeSaved.answerArray = answerArray
      objToBeSaved.courseMappingId = $state.params.courseMappingId;
 
      var statusHistory = [];
@@ -106,14 +111,13 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
      }
 
      var date = new Date();
-     statusHistory.push({changedFrom:scope.$parent.previewData.status, changedTo:submitStatus, changedBy:$rootScope.userinfo.ActiveUserData.roleMappingObj._id, changedOn:date});
+     statusHistory.push({changedFrom:scope.$parent.previewData.status, changedTo:submitStatus, changedBy:$rootScope.userinfo.ActiveUserData.roleMappingObj._id.$oid, changedOn:date});
 
      objToBeSaved.statusHistory = statusHistory;
-     objToBeSaved.penaltyHistory = penaltyHistory;
+     objToBeSaved.penaltyHistory = scope.penaltyHistory;
 
-
-     
-     var response = assignmentFunctions.fnSubmitAssignment(objToBeSaved);
+   
+    var response = assignmentFunctions.fnSubmitAssignment(objToBeSaved);
      
      response.then(function(response){
 
@@ -139,11 +143,11 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
 
 
      		if(angular.equals(submitStatus, 'draft')){
-			$alert({title:'Error',content:'Sorry, some error occurred in saving the current changes. Please retry', placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'danger', show:true});
+			$alert({title:'Error',content:'Sorry, some error occurred in saving the current changes. Please retry', placement:'top-right', duration:'8', animation:'am-fade-and-slide-bottom', type:'danger', show:true});
 
      		}
      		else{
-     			$alert({title:'Error',content:'Sorry, some error occurred in submitting the assignment, please retry', placement:'top-right', duration:'4', animation:'am-fade-and-slide-bottom', type:'danger', show:true});
+     			$alert({title:'Error',content:'Sorry, some error occurred in submitting the assignment, please retry', placement:'top-right', duration:'8', animation:'am-fade-and-slide-bottom', type:'danger', show:true});
      		}
 
      	}
@@ -155,166 +159,11 @@ angular.module('baabtra').directive('assignmentViewer',  ['$rootScope','$state',
 
  //_______________________________________________________________________________________________
 
-	// -----------------------------------------------------------------------------------------------------
 	//function to check whether penalty is applicable
-	var checkAndApplyPenalty = false;
-	var timeDiff = 0;
-    
-    var checkForPenalty = function() {		
-
-			
-
-				//checking if there is an assigned date
-				if(!angular.equals(scope.$parent.previewData.assignedDate,undefined)){		
-
-					var assignedDate = new Date(scope.$parent.previewData.assignedDate);
-					assignedDate = assignedDate.getTime();
-					
-					var sumbittedDate = new Date();
-					sumbittedDate = sumbittedDate.getTime();
-					
-					if(sumbittedDate > assignedDate){
-						checkAndApplyPenalty = true;
-						timeDiff = Math.abs(sumbittedDate - assignedDate);
-					}
-																																																											
-				
-				
-				} else{
-					
-					var sumbittedDate = new Date();
-					sumbittedDate = sumbittedDate.getTime();
-					
-					var courseAssignedDate = new Date(scope.$parent.previewData.courseAssignedDate);
-					courseAssignedDate = courseAssignedDate.getTime();
-					
-					var tlPoint = parseInt(scope.$parent.previewData.tlPointInMinute)*60*1000;
-					
-					if(sumbittedDate > (courseAssignedDate + tlPoint)){
-						checkAndApplyPenalty = true;
-						timeDiff = Math.abs(sumbittedDate - (courseAssignedDate + tlPoint));	
-				}
-					
-			}
-			
-			
-		
-	}
-// -----------------------------------------------------------------------------------------------------
-
-// function to apply penalty
-var penaltyHistory;
-var findPenalty = function() {	
-
-	penaltyHistory = [];
-
-    if (!angular.equals(scope.$parent.previewData.penaltyHistory,undefined) && !angular.equals(scope.$parent.previewData.penaltyHistory,null)){
-		penaltyHistory = angular.copy(scope.$parent.previewData.penaltyHistory);
-	}
-	
-	var penaltyArray = scope.data.value.penaltyArray;
-				
-
-
-	// a metrics for the calculation in accordance with the late time units 
-	var calculationMetrics = {hours:3600000,
-							  days:86400000,
-							  months:2592000000};	
-
-	//finding out the applicable penalty obj
-	for (var i in penaltyArray){
-		currentPenaltyObj =penaltyArray [i];
-		
-
-		var currentDuration = parseInt(currentPenaltyObj.lateTime)*(calculationMetrics[currentPenaltyObj.lateTimeUnits]);
-
-		if(angular.equals(scope.$parent.previewData.status,"to be resubmitted")){
-
-			if(angular.equals(currentPenaltyObj.submissionMode,"re-submitted")){
-				//pushing the penalty object into the penalty array
-				penaltyHistory.push(currentPenaltyObj);
-			}
-
-			if(angular.equals(currentPenaltyObj.submissionMode,"re-submission is late")){
-				if(timeDiff>currentDuration){
-						//pushing the penalty object into the penalty array
-						penaltyHistory.push(currentPenaltyObj);
-				}
-			}			
-
-			return;
-		}
-
-
-		if(timeDiff>currentDuration){			
-			//pushing the penalty object into the penalty array
-			penaltyHistory.push(currentPenaltyObj);
-		}
-	}
-
-	
-
-	
- 	
-	return;
-			
-}
-
-// -----------------------------------------------------------------------------------------------------
-
-//function to apply the penalty
-var applyPenalty = function(totalMarks){
-
-
-	
-
-	//calculating the reduction marks
-	var reduce = 0;
-
-	for (var i in penaltyHistory){
-	
-
-	var currentPenalty = penaltyHistory[i];
-
-		//if an exemption is set manually for applying the  skip that object
-    	if(!angular.equals(currentPenalty.exemptRule, undefined)){
-			if(currentPenalty.exemptRule){
-				continue;
-			}
-		}
-
-		if(currentPenalty.blockSubmission){
-			scope.blockSubmission = true;
-		}
-
-		
-
-		if(angular.equals(currentPenalty.penaltyCalculationUnit,"% of marks")){
-			
-			reduce = totalMarks*(currentPenalty.reductionUnits/100);
-		}
-		else if (angular.equals(currentPenalty.penaltyCalculationUnit,"times of marks")){
-			reduce = totalMarks*currentPenalty.reductionUnits;
-		}
-		else if (angular.equals(currentPenalty.penaltyCalculationUnit,"marks")){
-			reduce = currentPenalty.reductionUnits;
-		}
-
-		
-
-		//multiplying the reduction marks with the frequency
-		if(angular.equals(currentPenalty.penaltyCalculationUnit,"each day delayed")){
-			var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-			reduce = reduce*diffDays;
-		}
-
-		totalMarks = totalMarks - reduce;
-	}
-	
- 	
-	return totalMarks;	
-
-}
+	scope.checkAndApplyPenalty = false;
+	scope.timeDiff = 0;
+    // array to hold the penalty history
+	scope.penaltyHistory = [];
 // -----------------------------------------------------------------------------------------------------
 
 //function to block the submission if penalty is applicable
@@ -324,9 +173,9 @@ var fnBlockSubmission = function(){
 
 	var currentAppliedPenalty = {};
     
-    for (var i in penaltyHistory) {
+    for (var i in scope.penaltyHistory) {
 
-    	currentAppliedPenalty = penaltyHistory[i];
+    	currentAppliedPenalty = scope.penaltyHistory[i];
 
     	//if an exemption is set manually for blocking submission skip that object
     	if(!angular.equals(currentAppliedPenalty.exemptBlocking, undefined)){
@@ -359,19 +208,32 @@ var fnBlockSubmission = function(){
 			}
 
 // -----------------------------------------------------------------------------------------------------
+//if block submission is true, don't show the penalty frequency
+			scope.fnShowPenaltyFrequency = function(rule){
 
+				if (rule.blockSubmission) {
+					return "";
+				}
+				else{
+					return "for" + rule.penaltyFrequency;
+				}
+				
+			}
+// -----------------------------------------------------------------------------------------------------
 
 
 //check for penalty in the assignment level
-checkForPenalty();
+assignmentFunctions.checkForPenalty(scope);
+
+
 
 //if penalty is applicable, find the applicable penalties
-if(checkAndApplyPenalty){
-	findPenalty();
+if(scope.checkAndApplyPenalty){
+	assignmentFunctions.findPenalty(scope);
 }
 
 //if there are penalties applicable, block the submission if the rule implies blocking of submission
-if(penaltyHistory.length > 0){
+if(scope.penaltyHistory.length > 0){
 	fnBlockSubmission();
 }
 
