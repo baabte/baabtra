ca126329884e25af2d6a8ecb8640022d29140b53
arunbaabte <arun@baabte.com>
2015-06-29 19:24:24 +0530

    commit with changes in ui of random exam and test

--- config.js
+++ config.js
@@ -63,8 +63,8 @@ angular.module('baabtra')
   // }])
   .constant('bbConfig',{ //used for storing enviornment variables
 
-     "BWS": "http://127.0.0.1:8000/",//local
-    // "BWS": "http://services.baabtra.com/",//server - production
+    // "BWS": "http://127.0.0.1:8000/",//local
+     "BWS": "http://services.baabtra.com/",//server - production
     // "BWS": "http://server.baabtra.in/",//server-test
     // "BWS": "http://server.mb-test.in/",//server-test
 
@@ -77,9 +77,8 @@ angular.module('baabtra')
 angular.module('angular-custom-form',[]) /*Config constant for angular custom form */
 .constant('bbConfig',{ //used for storing enviornment variables
 
-     "BWS": "http://127.0.0.1:8000/",//local
-     //"BWS": "http://services.baabtra.com/",//server - production
-    // "BWS": "http://server.mb-test.in/",//server-test
+     // "BWS": "http://127.0.0.1:8000/",//local
+     "BWS": "http://services.baabtra.com/",//server - production
      "SARID":1, // SuperAdminRoleID
      "CURID":2, // CompanyUser
      "MURID":3, // MenteeUser
