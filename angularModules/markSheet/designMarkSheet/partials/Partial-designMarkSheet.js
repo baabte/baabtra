45683313e39c3cc9d23533ed2a4213e7d3ca7317
lijin <lijin@baabte>
2015-07-05 10:23:53 +0530

    local change

--- angularModules/markSheet/designMarkSheet/partials/Partial-designMarkSheet.js
+++ angularModules/markSheet/designMarkSheet/partials/Partial-designMarkSheet.js
@@ -168,7 +168,8 @@ $scope.$watch('data.selectedNode',function () {
 
 			if(angular.equals($scope.data.selectedNode.mark.type,'mark')){
 				$scope.data.markType='mark';
-				if(elem.evaluable&&!angular.equals(elem.totalMark,undefined)){
+				// elem.evaluable&&
+				if(!angular.equals(elem.totalMark,undefined)){
 					$scope.elementsOfSelectedNode.push({element:elem,key:$scope.data.selectedNode.element[key]});
 				}
 			}
