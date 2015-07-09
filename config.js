5e858f3f5ba468893d046b0061fa128692b46b76
Lijin <lijin@baabte.com>
2015-07-09 13:12:00 +0530

    local changes

--- config.js
+++ config.js
@@ -63,10 +63,10 @@ angular.module('baabtra')
   // }])
   .constant('bbConfig',{ //used for storing enviornment variables
 
-    "BWS": "http://127.0.0.1:8000/",//local
+    //"BWS": "http://127.0.0.1:8000/",//local
 
     // "BWS": "http://server.baabtra.in/",//server-test
-    // "BWS": "http://server.mb-test.in/",//server-test
+     "BWS": "http://server.mb-test.in/",//server-test
 
      "SARID":1, // SuperAdminRoleID
      "CURID":2, // CompanyUser
@@ -79,8 +79,8 @@ angular.module('baabtra')
 angular.module('angular-custom-form',[]) /*Config constant for angular custom form */
 .constant('bbConfig',{ //used for storing enviornment variables
 
-      "BWS": "http://127.0.0.1:8000/",//local
-     //"BWS": "http://services.baabtra.com/",//server - production
+      //"BWS": "http://127.0.0.1:8000/",//local
+     "BWS": "http://services.baabtra.com/",//server - production
 
      "SARID":1, // SuperAdminRoleID
      "CURID":2, // CompanyUser
