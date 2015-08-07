ed748da0035a07485eec69294e79fe0e8ba94947
jihin <jihin@baabte.com>
2015-08-07 16:59:40 +0530

    baabtra profile by course by Jihin

--- angularModules/course/directives/Directive-scoreCard.js
+++ angularModules/course/directives/Directive-scoreCard.js
@@ -44,27 +44,26 @@ angular.module('baabtra').directive('scoreCard',['$compile', function($compile) 
 					if(syllabus[index].children.length){
 						for (var i = 0; i < syllabus[index].children.length; i++) {
 							var mark = getMarkInAllLevel(syllabus[index].children, i);
-							if(angular.equals(syllabus[index].mark.markScored,undefined)){
-              					syllabus[index].mark.markScored = 0;
-           					}
-           					if(!angular.equals(mark.markScored, undefined)){
-           						
-           						if(mark.markScored > 0){
-           							mark.markScored = parseFloat(mark.markScored).toFixed(2);
-	           						syllabus[index].mark.markScored = parseFloat(syllabus[index].mark.markScored) + parseFloat(mark.markScored);
-	           						//console.log(syllabus[index].mark.markScored);
-	           					}
-
+							var length = syllabus[index].children.length;
+							if(!syllabus[index].mark.markScored){
+								syllabus[index].mark.markScored = 0;
+							}
+							if(mark.markScored){
+								syllabus[index].mark.markScored = syllabus[index].mark.markScored + (mark.markScored/mark.maxMark);
+								if(angular.equals(length, (i+1))){
+									syllabus[index].mark.markScored = parseFloat(((syllabus[index].mark.markScored/length)*syllabus[index].mark.maxMark).toFixed(2));
+								}
 							}
 						}
-
-						syllabus[index].mark.markScored = syllabus[index].mark.markScored/syllabus[index].children.length;
-						
-						syllabus[index].mark.markScored = syllabus[index].mark.markScored.toFixed(2);
 					}
-				      if(syllabus[index].mark){
-				        return syllabus[index].mark;
-				      }
+
+				    if(syllabus[index].mark){
+				    	if(syllabus[index].mark.markScored){
+				    		syllabus[index].mark.markScored = parseFloat(syllabus[index].mark.markScored.toFixed(2));
+				    	}
+				    	return syllabus[index].mark;
+				    }
+
 				}
 			}
 
@@ -74,3 +73,20 @@ angular.module('baabtra').directive('scoreCard',['$compile', function($compile) 
 		}
 	};
 }]);
+
+
+/*
+if(angular.equals(syllabus[index].mark.markScored,undefined)){
+              					syllabus[index].mark.markScored = 0;
+           					}
+           					if(!angular.equals(mark.markScored, undefined)){
+           						
+           						if(mark.markScored > 0){
+           							mark.markScored = parseFloat(mark.markScored).toFixed(2);
+	           						//syllabus[index].mark.markScored = parseFloat(syllabus[index].mark.markScored) + parseFloat(mark.markScored);
+	           						console.log(syllabus[index].mark.markScored);
+	           						//console.log(syllabus[index].mark.markScored);
+	           					}
+
+							}
+*/
