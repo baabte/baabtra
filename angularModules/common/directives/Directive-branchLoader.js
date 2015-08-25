0ce7cd67a8a033ca7f041f9651a63f6b19afe473
jihin <jihin@baabte.com>
2015-08-25 17:37:06 +0530

    branch multi select by Jihin

--- angularModules/common/directives/Directive-branchLoader.js
+++ angularModules/common/directives/Directive-branchLoader.js
@@ -4,17 +4,18 @@ Created By : Jihin
 Created On : 25/08/2015
 */
 angular.module('baabtra').directive('branchLoader',['branchSrv','manageTreeStructureSrv','$rootScope',function(branchSrv,manageTreeStructureSrv,$rootScope) {
-	return {
-		restrict: 'E',
-		require:["ng-model"],
-		scope: {
-			ngModel:"=",
+  return {
+    restrict: 'E',
+    require:["ng-model"],
+    scope: {
+      ngModel:"=",
       onBranchRemove:"=?",
-      onBranchChange:"=?"
-		},
-		templateUrl: 'angularModules/common/directives/Directive-branchLoader.html',
+      onBranchChange:"=?",
+      multiple:"=?"
+    },
+    templateUrl: 'angularModules/common/directives/Directive-branchLoader.html',
 
-		link: function($scope, element, attrs, ctrls) {
+    link: function($scope, element, attrs, ctrls) {
       
       var rm_id = $rootScope.userinfo.ActiveUserData.roleMappingId.$oid;
       var roleId = $rootScope.userinfo.ActiveUserData.roleMappingObj.fkRoleId;
@@ -23,27 +24,40 @@ angular.module('baabtra').directive('branchLoader',['branchSrv','manageTreeStruc
       var branchCondition = {companyId:companyId};
       $scope.branchObj = {};
       $scope.branchObj.loadedBranches = [];
+      
 
-      if($scope.ngModel){
-        branchCondition._id = $scope.ngModel.$oid?$scope.ngModel.$oid:$scope.ngModel;
+      if($scope.multiple){
+        var branchLoaderResponse = branchSrv.fnLoadAllBranchesUnderCompany(branchCondition);
+        branchLoaderResponse.then(function(response){
+           $scope.branchObj.branch = angular.fromJson(JSON.parse(response.data));
+           for(var branch in $scope.branchObj.branch){
+            $scope.branchObj.branch[branch]._id = $scope.branchObj.branch[branch]._id.$oid;
+           }
+        });
       }
       else{
-        branchCondition.parent = "root";
-      }
-      
-      var branchLoadResponse = branchSrv.fnLoadBranch(branchCondition);
-      branchLoadResponse.then(function(response){
-        var result = angular.fromJson(JSON.parse(response.data));
+        
         if($scope.ngModel){
-          $scope.ngModel = $scope.ngModel.$oid?$scope.ngModel.$oid:$scope.ngModel;
-          $scope.branchObj.currentBranch = {_id:result[0]._id.$oid, name:result[0].name};
+          branchCondition._id = $scope.ngModel.$oid?$scope.ngModel.$oid:$scope.ngModel;
         }
         else{
-          $scope.branchObj.branch = result;
-          $scope.branchObj.loadedBranches.push(result);
+          branchCondition.parent = "root";
         }
-        
-      });
+
+        var branchLoadResponse = branchSrv.fnLoadBranch(branchCondition);
+        branchLoadResponse.then(function(response){
+          var result = angular.fromJson(JSON.parse(response.data));
+          if($scope.ngModel){
+            $scope.ngModel = $scope.ngModel.$oid?$scope.ngModel.$oid:$scope.ngModel;
+            $scope.branchObj.currentBranch = {_id:result[0]._id.$oid, name:result[0].name};
+          }
+          else{
+            $scope.branchObj.branch = result;
+            $scope.branchObj.loadedBranches.push(result);
+          }
+          
+        });
+      }
 
       $scope.removeBranch = function (currentBranch) {
 
@@ -85,9 +99,6 @@ angular.module('baabtra').directive('branchLoader',['branchSrv','manageTreeStruc
           }
         }
 
-        
-        
-
         if(item.children.length){
           branchCondition = {companyId:companyId, parent:item._id.$oid};
           var branchLoadResponse = branchSrv.fnLoadBranch(branchCondition);
@@ -101,8 +112,17 @@ angular.module('baabtra').directive('branchLoader',['branchSrv','manageTreeStruc
 
       };//branchSelected-fn-end
 
+      $scope.multipleBranchSelected = function(item, index){
+        $scope.ngModel.push(item);
+      };
+
+      $scope.multipleBranchRemove = function(item, index){
+        var index = $scope.ngModel.indexOf(item);
+        $scope.ngModel.splice(index, 1);
+      };
+
     }//link-end
 
-	};
+  };
 
 }]);
